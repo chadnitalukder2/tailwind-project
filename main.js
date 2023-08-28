9158e2e0d5e4de04d089c4f6fbed4e9cf63923e3
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-08-28 21:44:05 +0600

    MENU-COM

--- main.js
+++ main.js
@@ -20,6 +20,10 @@ navMenu.classList.remove('hidden')
 
 /*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
 const tabs = document.querySelectorAll(".tabs__wrap ul li");
+const all = document.querySelectorAll('.item_wrap');
+const foods = document.querySelectorAll('.food');
+const beverages = document.querySelectorAll('.beverage');
+const snacks = document.querySelectorAll('.snack');
 
 tabs.forEach(tab => {
     tab.addEventListener("click",() => {
@@ -27,6 +31,33 @@ tabs.forEach(tab => {
             tab.classList.remove("active")
         })
         tab.classList.add('active')
+        const tabval = tab.getAttribute('data-tabs')
+    all.forEach(item =>{
+        item.style.display = 'none'
+    })
+    
+        if (tabval == 'food'){
+            foods.forEach(item =>{
+                item.style.display ='block'
+            })
+        }
+        else if (tabval == 'snack'){
+            snacks.forEach(item =>{
+                item.style.display ='block'
+            })
+        }
+        else if (tabval == "beverage") {
+            beverages.forEach((item) => {
+              item.style.display = "block";
+            });
+        }
+        else{
+            all.forEach(item =>{
+                item.style.display ='block'
+            })
+        }
+
+
     })
 })
 
